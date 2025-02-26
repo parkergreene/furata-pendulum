2905d2e754387fd75205f20f25145680028eb807
Parker Greene <parkergreene16@gmail.com>
2025-02-25 21:08:11 -0800

    added PID code

--- furata_pendulum_firmware/src/main.cpp
+++ furata_pendulum_firmware/src/main.cpp
@@ -17,6 +17,25 @@ long encoderZero = 0;  // Variable to store the initial position as zero referen
 const int stepsPerRevolution = 200;
 AccelStepper stepper(1,STEP_PIN, DIR_PIN);
 
+//PID setup
+float kp = 1;
+float ki = 0.005;
+float kd = 0.000;
+float bias = 0;
+float setpoint = 90;
+
+float error_prev = 0;
+float integral = 0;
+float derivative = 0;
+float move_deg = 0;
+float error = 0;
+
+unsigned long timestamp = 0;
+unsigned long timestamp_prev = 0;
+int counter = 0;
+
+const double enc_guard_min = 60;
+const double enc_guard_max = 120;
 
 void setup() {
 
@@ -30,23 +49,40 @@ void setup() {
   stepper.setAcceleration(10000);
 
 }
-
-
-
 //float encoderPrev = getEncoderPosition(encoder, encoderZero, pulsePerRev);
 
 void loop() {
   
+  stepper.move(move_deg);
+
   float encPosition = getEncoderPosition(encoder, encoderZero, pulsePerRev);
-  
+  timestamp = millis();
+  counter++;
   //float encChange = encPosition - encoderPrev;
 
   //float move = degsToSteps(encChange, stepsPerRevolution);
-
-  Serial.println(encPosition, 2);
-
   //stepper.move(move);
   //stepper.run();
+
+  float interval = timestamp - timestamp_prev;
+  timestamp_prev = timestamp; 
+
+
+  error = setpoint - encPosition;
+  integral = integral + (error * interval);
+  derivative = (error - error_prev) / interval;
+  move_deg = (kp * error) + (ki * integral) + (kd * derivative) + bias;
+
+  error_prev = error;
+
+
+  //Serial.println(encPosition, 2);
+  Serial.println("Encoder Position:" + String(encPosition) + "| move_deg"+ String(move_deg));
+
+  //if (encPosition < enc_guard_min || encPosition > enc_guard_max){
+  //  break;
+  //}
+
 
   //encoderPrev = encPosition;
   //Serial.println(encPosition, 2);
